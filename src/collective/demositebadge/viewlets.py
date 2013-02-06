4dab2b2c5f2ea3dd1d2099763a5dc5f758845c5f
victor <victor@victor-TravelMate-5760G.(none)>
2013-02-06 18:01:31 +0200

    shows and hides the image

--- src/collective/demositebadge/viewlets.py
+++ src/collective/demositebadge/viewlets.py
@@ -1,9 +1,20 @@
 from plone.app.layout.viewlets import common as base
+from zope.component import getMultiAdapter
+from Acquisition import aq_inner
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
 
 
 
 class DemoSiteBadgeViewlet(base.ViewletBase):
 
-    def show_title(self):
-        string = ""
-        return string
+    def available(self):
+        context = aq_inner(self.context)
+        portal = getMultiAdapter((context, self.request),
+            name=u'plone_portal_state').portal()
+        registry = getUtility(IRegistry)
+        
+        if registry['collective.demositebadge.interfaces.IBadgeSettings.check']:
+            return 1
+        else:  
+            return 0
