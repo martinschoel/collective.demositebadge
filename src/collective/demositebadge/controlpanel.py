512a1b2ecbab527cfc433cfdf472e27b3fac5e1b
victor <victor@victor-TravelMate-5760G.(none)>
2013-02-15 19:01:55 +0200

    control panel class

---
+++ src/collective/demositebadge/controlpanel.py
@@ -0,0 +1,17 @@
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+
+from plone.z3cform import layout
+from z3c.form import form, button
+from collective.demositebadge.interfaces import IBadgeSettings
+from Products.statusmessages.interfaces import IStatusMessage
+
+
+class ControlPanelForm(RegistryEditForm):
+    
+    form.extends(RegistryEditForm)
+    schema = IBadgeSettings
+    
+    
+ControlPanelView = layout.wrap_form(ControlPanelForm, ControlPanelFormWrapper)
+ControlPanelView.label = u"Demo site badge"
