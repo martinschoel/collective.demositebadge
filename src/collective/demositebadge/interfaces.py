fb249231e081e9baa4a4f6b262689bc70484b5d4
victor <victor@victor-TravelMate-5760G.(none)>
2013-02-19 19:16:38 +0200

    Badge title changed. Corrected formatting.

--- src/collective/demositebadge/interfaces.py
+++ src/collective/demositebadge/interfaces.py
@@ -3,6 +3,6 @@ from zope import schema
 
 
 class IBadgeSettings(Interface):
-    
-    check = schema.Bool(title=u'Display badge',default=False)
-    text = schema.TextLine(title=u'Text to show',required=False,default=u'Demo site')
+    check = schema.Bool(title=u'Display badge', default=False)
+    text = schema.TextLine(title=u'Badge label', required=False,               
+        default=u'Demo site')
