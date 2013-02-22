8f524ec6ff82a998e8a5cee0181c0df68bb2c209
victor <victor@victor-TravelMate-5760G.(none)>
2013-02-22 15:18:40 +0200

    tests for view

---
+++ src/collective/demositebadge/tests/test_view.py
@@ -0,0 +1,72 @@
+import unittest2 as unittest
+
+from Products.CMFCore.utils import getToolByName
+from plone.app.testing import ploneSite
+from plone.app.testing import applyProfile , quickInstallProduct
+from AccessControl import getSecurityManager
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from Acquisition import aq_inner
+
+from collective.demositebadge.views import DemoSiteBadgeView
+
+
+from collective.demositebadge.testing import \
+    COLLECTIVE_DEMOSITEBADGE_INTEGRATION_TESTING
+
+
+# TODO: add proper name to testing class
+class TestExample(unittest.TestCase):
+
+    layer = COLLECTIVE_DEMOSITEBADGE_INTEGRATION_TESTING
+
+    def setUp(self):
+        # you'll want to use this to set up anything you need for your tests
+        # below
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.view = DemoSiteBadgeView(request=self.request,  
+            context=self.portal)
+
+    
+    
+    def test_get_demo_label(self):
+        res = self.view.get_demo_label()
+        registry = getUtility(IRegistry)
+        self.assertEqual(registry[
+            'collective.demositebadge.interfaces.IBadgeSettings.text'], res)     
+
+        
+    def test_available(self):
+        registry = getUtility(IRegistry)
+        
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+            True
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+            u'Testing site'
+        result = self.view.available()
+        self.assertTrue(result)
+        
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+            False
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+            u'Testing site'
+        result = self.view.available()
+        self.assertFalse(result)
+        
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+            True
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+            u''
+        result = self.view.available()
+        self.assertFalse(result)
+        
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+            False
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+            u''
+        result = self.view.available()
+        self.assertFalse(result)
+
