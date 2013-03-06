1ce4c8fcb8a9fbe954a5ec7f666f1999d037922f
victor <victor@victor-TravelMate-5760G.(none)>
2013-03-06 18:19:24 +0200

    Removed "get_demo_label" method.

--- src/collective/demositebadge/views.py
+++ src/collective/demositebadge/views.py
@@ -10,21 +10,16 @@ from Acquisition import aq_inner
 class DemoSiteBadgeView(BrowserView):
     
     def available(self):
-        registry = getUtility(IRegistry)
-    
-        if registry[\
-                'collective.demositebadge.interfaces.IBadgeSettings.check'] \
-           and registry[\
-                'collective.demositebadge.interfaces.IBadgeSettings.text']:
-            return True
-        else:
-            return False
-    
-    def get_demo_label(self):
         context = aq_inner(self.context)
         portal = getMultiAdapter((context, self.request),
             name=u'plone_portal_state').portal()
         registry = getUtility(IRegistry)
         text = registry[\
             'collective.demositebadge.interfaces.IBadgeSettings.text']
-        return text
+    
+        if registry[\
+                'collective.demositebadge.interfaces.IBadgeSettings.check'] \
+           and text:
+            return text
+        else:
+            return ''
