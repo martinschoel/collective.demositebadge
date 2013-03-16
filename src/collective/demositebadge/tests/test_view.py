8899c6c19315fd7ee7a58d265db141aee1cf5d3d
Vitaliy Podoba <vitaliypodoba@gmail.com>
2013-03-16 16:12:44 +0200

    update tests

--- src/collective/demositebadge/tests/test_view.py
+++ src/collective/demositebadge/tests/test_view.py
@@ -1,24 +1,14 @@
 import unittest2 as unittest
 
-from Products.CMFCore.utils import getToolByName
-from plone.app.testing import ploneSite
-from plone.app.testing import applyProfile , quickInstallProduct
-from AccessControl import getSecurityManager
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
-from Acquisition import aq_inner
 
 from collective.demositebadge.views import DemoSiteBadgeView
-
-
 from collective.demositebadge.testing import \
     COLLECTIVE_DEMOSITEBADGE_INTEGRATION_TESTING
 
 
-# TODO: add proper name to testing class
-class TestView(unittest.TestCase):
+class DemoSiteBadgeViewTestCase(unittest.TestCase):
 
     layer = COLLECTIVE_DEMOSITEBADGE_INTEGRATION_TESTING
 
@@ -27,39 +17,41 @@ class TestView(unittest.TestCase):
         # below
         self.portal = self.layer['portal']
         self.request = self.layer['request']
-        self.view = DemoSiteBadgeView(request=self.request,  
+        self.view = DemoSiteBadgeView(request=self.request,
             context=self.portal)
 
-    
-    
     def test_get_demo_label(self):
         registry = getUtility(IRegistry)
 
-        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] = \
             True
-        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] = \
             u'Testing site'
         result = self.view.get_demo_label()
         self.assertEqual(result,'Testing site')
 
-        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] = \
             False
-        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] = \
             u'Testing site'
         result = self.view.get_demo_label()
         self.assertEqual(result,'')
 
-        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] = \
             True
-        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] = \
             u''
         result = self.view.get_demo_label()
         self.assertEqual(result,'')
 
-        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.check'] = \
             False
-        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] =\
+        registry['collective.demositebadge.interfaces.IBadgeSettings.text'] = \
             u''
         result = self.view.get_demo_label()
         self.assertEqual(result,'')
 
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(DemoSiteBadgeViewTestCase))
+    return suite
